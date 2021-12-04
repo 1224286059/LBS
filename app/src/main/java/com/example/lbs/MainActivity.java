49d875bcb131397bb9c3e87e8079c9cfbdc76834
1224286059 <1224286059@qq.com>
2021-12-04 21:20:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lbs/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.lbs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView =null;
+    private BaiduMap mBaiduMap=null;
+    private LocationClient mLocationClient=null;
+    private boolean isFirstLocation = true;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        //卫星地图
+        mMapView.getMap().setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+        mBaiduMap=mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+
+        mLocationClient = new LocationClient(this);
+
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+        mLocationClient.start();
+
+    }
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLocation) {
+                // 获取经纬度
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatusUpdate status = MapStatusUpdateFactory.newLatLng(ll);
+                // mBaiduMap.setMapStatus(status);//直接到中间
+                mBaiduMap.animateMapStatus(status);// 动画的方式到中间
+                isFirstLocation = false;
+
+            }
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+}
